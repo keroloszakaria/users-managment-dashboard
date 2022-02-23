2b09ecbf12cddc502b016eb51be72c2ddc1320c6
unknown <keroloszakaria2@gmail.com>
2022-02-23 23:40:43 +0200

    add All Dependencies I will need

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import router from "./router";
+import store from "./store";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// Import Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
+const app = createApp(App).use(store).use(router);
+app.mount("#app");
